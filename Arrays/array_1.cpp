be77cdf2d116a840993deedc47c7fb61f9298782
LoG1604 <lakshayguptabsr@gmail.com>
2024-06-23 15:38:10 +0530

    Array file edited

--- Arrays/array_1.cpp
+++ Arrays/array_1.cpp
@@ -5,7 +5,7 @@ int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]); // Calculate the size of the array
 
-    // Traverse the array and print each element
+    // Traverse the array and print each element edit
     std::cout << "Array elements: ";
     for (int i = 0; i < n; ++i) {
         std::cout << arr[i] << " ";
